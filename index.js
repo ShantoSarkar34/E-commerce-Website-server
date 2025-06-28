550a903ea59d45dc5f5f41c89bf4f4d0bb05cae0
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-06-28 17:42:58 +0600

    created shopO server using mondodb,express,nodejs and successfully pinged

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@shopo-server.putq3dw.mongodb.net/?retryWrites=true&w=majority&appName=ShopO-Server`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("AllProducts");
+    const productsCollection = database.collection("products");
+
+    app.get("/all-products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("ShopO server is active now...!");
+});
+
+app.listen(port, () => {
+  console.log(`ShopO server is running on port ${port}`);
+});
